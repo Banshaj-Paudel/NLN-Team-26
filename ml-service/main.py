054aec04eb3e617253128491a6ee0df317614637
Banshaj-Paudel <bhairutribrid@gmail.com>
2026-03-29 18:18:49 +0545

    ML Endpoint Exposed

--- ml-service/main.py
+++ ml-service/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from predictor import predict_risk
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class CheckIn(BaseModel):
     sleep_hours: float
